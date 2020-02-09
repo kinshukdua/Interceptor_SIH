d62a5c6c533f4f356f4044ac8bf8ef646b20991e
Kinshuk Dua <kinshukduaexam@gmail.com>
2020-02-09 20:59:22 +0530

    restruct missile and interceptor as classes

--- interceptor.py
+++ interceptor.py
@@ -2,7 +2,80 @@ from matplotlib import pyplot as plt
 import numpy as np
 from scipy import optimize as opt
 
-range_ = 109
+range_ = np.sqrt(109)
+class Interceptor:
+    def __init__(self,latitude,longitude,range_):
+        self.x = latitude
+        self.y = longitude
+        self.r = range_
+    def get_x(self):
+        return self.x
+    def get_y(self):
+        return self.y
+    def in_range(self,missile):
+        x0 = self.x
+        y0 = self.y
+        x1 = missile.get_x()
+        y1 = missile.get_y()
+        z1 = missile.get_z()
+        r = self.r
+        return (x1-x0)**2+(y1-y0)**2+z1**2 <= r**2
+class Missile: 
+    def __init__(self,latitude,longitude,altitude):
+        self.x = latitude
+        self.y = longitude
+        self.z = altitude
+    def get_x(self):
+        return self.x
+    def get_y(self):
+        return self.y
+    def get_z(self):
+        return self.z
+
+def exp(x,a,b,c):
+    return a * np.exp(-b * x) + c
+def exp_prime(x,a,b,c):
+    return -a * b * np.exp(-b * x) + c
+
+interceptors = [(8.62, 1.92),
+ (0.44, 3.68),
+ (8.59, 9.29),
+ (8.64, 4.74),
+ (2.32, 7.25),
+ (9.96, 1.09),
+ (9.1, 5.35),
+ (3.83, 2.39),
+ (1.51, 5.73),
+ (5.97, 3.17)]
+
+missiles = [(0.29, 3.32, 8.78),
+ (6.39, 0.62, 4.08),
+ (5.44, 8.41, 8.66),
+ (1.2, 6.22, 4.94),
+ (7.87, 5.05, 3.85),
+ (8.17, 3.59, 9.28),
+ (0.41, 2.77, 8.58),
+ (9.1, 0.16, 7.85),
+ (1.22, 3.15, 4.24),
+ (8.99, 8.61, 0.43),
+ (1.39, 0.68, 8.6),
+ (4.35, 3.29, 2.34),
+ (4.75, 0.99, 0.91),
+ (8.34, 0.48, 6.34),
+ (9.56, 6.7, 6.75),
+ (2.8, 6.24, 1.89),
+ (9.59, 6.32, 0.93),
+ (6.12, 9.7, 0.29),
+ (7.67, 1.09, 4.69),
+ (7.88, 9.42, 4.22)]
+ 
+interceptors = [Interceptor(i[0],i[1],range_) for i in interceptors]
+missiles = [Missile(m[0],m[1],m[2]) for m in missiles]
+
+
+
+"""
+
 sums = []
 sums_inv = []
 ns = []
@@ -32,7 +105,7 @@ interceptors = [(8.62, 1.92),
  (3.83, 2.39),
  (1.51, 5.73),
  (5.97, 3.17)]
-dic = {i:[]for i in interceptors}
+dic = {i:[] for i in interceptors}
 missiles = [(0.29, 3.32, 8.78),
  (6.39, 0.62, 4.08),
  (5.44, 8.41, 8.66),
@@ -81,9 +154,9 @@ if flag:
         sum_ = 0
         for e in x[:n]:
             for m in e[1]:
-                """
-                IMPORTANT VARIABLE m_prob[] == ????
-                """
+                
+                #IMPORTANT VARIABLE m_prob[] == ????
+                
                 m_prob[m] += 1
         ns.append(n)
         for i in m_prob.values():
@@ -124,3 +197,4 @@ else:
 #master = [ns,sums,delta+[0],deltadelta+[0,0],deltadeltadelta+[0,0,0]]
 #print(max(deltadeltadelta))
 
+"""
